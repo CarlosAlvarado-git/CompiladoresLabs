8444064c9b003022e1d7603e209d8ca0cf4ee310
Javier Mazariegos <javiermazariegos@ufm.edu>
2022-11-27 20:34:15 -0600

    ya funciona makefile

--- grupo3/class/src/Makefile.bat
+++ grupo3/class/src/Makefile.bat
@@ -1,2 +1,4 @@
-javac -cp "JFlex.jar;java-cup-11a-RELEASE150.jar" Compiler.java
-START " "
+javac -cp ".;JFlex.jar;java-cup-11a-RELEASE150.jar;java-cup-11a-runtime.jar" Compiler.java
+mkdir src
+copy entrada.txt src
+java -cp ".;JFlex.jar;java-cup-11a-RELEASE150.jar;java-cup-11a-runtime.jar" Compiler
