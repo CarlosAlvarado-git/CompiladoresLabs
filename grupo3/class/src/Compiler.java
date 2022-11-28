dd7c7a13d778b17a1ddad36101be486927cc5ee5
Javier Mazariegos <javiermazariegos@ufm.edu>
2022-11-28 00:40:53 -0600

    Update Compiler.java
    cambios en Compiler

--- grupo3/class/src/Compiler.java
+++ grupo3/class/src/Compiler.java
@@ -69,18 +69,41 @@ public class Compiler {
                             if (val == 0) {
                                 switch (tages[i]){
                                     case "target":
-                                        tag = tag.substring(6, tag.length());
+                                        //tag = tag.substring(6, tag.length());
                                         String ruta = "";
                                         //System.out.println(tag);
-                                        switch (tag){
+                                        String vacio = tag.substring(6,tag.length());
+                                        if(vacio == ""){
+                                            ruta = System.getProperty("user.dir");
+                                            ruta = ruta + "/src/" + archivo;
+                                            //Ruta al archivo
+                                            ruta = System.getProperty("user.dir");
+                                            ruta = ruta + "/src/" + archivo;
+                                            //Semantic
+                                            IRT_C irt_ = new IRT_C();
+                                            irt_.main(null,ruta);   
+                                            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                                            System.out.println("stage: scanning");
+                                            System.out.println("stage: parsing");
+                                            System.out.println("stage: ast");
+                                            System.out.println("stage: semantic");
+                                            System.out.println("stage: irt");
+                                            System.out.println("Irt completado");
+                                        }
+                                        else{
+                                        switch (vacio){
+                                            
                                             case "scan":
                                                 //Ruta al archivo
                                                 ruta = System.getProperty("user.dir");
                                                 ruta = ruta + "/src/" + archivo;
                                                 //Scanner
                                                 Scanner_ scan = new Scanner_();
-                                                System.out.println("Entre a scan");
+                                                //System.out.println("Entre a scan");
                                                 scan.main(null,ruta);
+                                                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                                                System.out.println("stage: scanning");
+                                                System.out.println("Scanning completado");
                                                 break;
                                             case "parse":
                                                 ruta = System.getProperty("user.dir");
@@ -88,6 +111,10 @@ public class Compiler {
                                                 //Scanner
                                                 Parser parse = new Parser();
                                                 parse.main(null,ruta);
+                                                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                                                System.out.println("stage: scanning");
+                                                System.out.println("stage: parsing");
+                                                System.out.println("Parsing completado");
                                                 break;
                                             case "ast":
                                                 ruta = System.getProperty("user.dir");
@@ -95,6 +122,11 @@ public class Compiler {
                                                 //Scanner
                                                 Ast ast = new Ast();
                                                 ast.main(null,ruta);
+                                                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                                                System.out.println("stage: scanning");
+                                                System.out.println("stage: parsing");
+                                                System.out.println("stage: ast");
+                                                System.out.println("Ast completado");
                                                 break;
                                             case "semantic":
                                                 ruta = System.getProperty("user.dir");
@@ -102,6 +134,12 @@ public class Compiler {
                                                 //Semantic
                                                 Semantic semantic = new Semantic();
                                                 semantic.main(null,ruta);
+                                                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                                                System.out.println("stage: scanning");
+                                                System.out.println("stage: parsing");
+                                                System.out.println("stage: ast");
+                                                System.out.println("stage: semantic");
+                                                System.out.println("semantic completado");
                                                 break;
                                             case "irt":
                                                 ruta = System.getProperty("user.dir");
@@ -109,10 +147,19 @@ public class Compiler {
                                                 //Semantic
                                                 IRT_C irt = new IRT_C();
                                                 irt.main(null,ruta);   
-                                                
+                                                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                                                System.out.println("stage: scanning");
+                                                System.out.println("stage: parsing");
+                                                System.out.println("stage: ast");
+                                                System.out.println("stage: semantic");
+                                                System.out.println("stage: irt");
+                                                System.out.println("Irt completado");
                                                 break;
                                             case "codegen":
                                                 break;
+                                            default:
+                                            break;
+                                        }
                                         }
                                         
                                         break;
@@ -121,11 +168,93 @@ public class Compiler {
                                     case "opt":
                                         break;
                                     case "debug":
-                                        break;
-                                    default:
-                                        break;
+                                        tag = tag.substring(5, tag.length());
+                                        ruta = "";
+                                        //System.out.println(tag);
+                                         for (int x = 0; x < tag.length(); x++){
+                                             int dos_puntos = tag.indexOf(":", x);
+                                             int verificacion = tag.indexOf(":");
+                                             int verificacion2 = tag.substring(x,tag.length()).indexOf(":");
+                                            
+                                             if (dos_puntos != -1 || verificacion == -1 || verificacion2 == -1){
+                                                 String tag_n = "";
+                                                 if(verificacion == -1){
+                                                     tag_n = tag;
+                                                 }
+                                                 else if(verificacion2 == -1){
+                                                     tag_n = tag.substring(x,tag.length());
+                                                 }
+                                                 else{
+                                                     tag_n = tag.substring(x, dos_puntos); 
+                                                 }
+                                                   
+                                            switch (tag_n){
+                                                case "scan":
+                                                    //Ruta al archivo
+                                                    ruta = System.getProperty("user.dir");
+                                                    ruta = ruta + "/src/" + archivo;
+                                                    //Scanner
+                                                    Scanner_ scan = new Scanner_();
+                                                    System.out.println("Debugging: scan");
+                                                    scan.main(null,ruta);
+                                                    System.out.println("Scanning completado");
+                                                    break;
+                                                case "parse":
+                                                    ruta = System.getProperty("user.dir");
+                                                    ruta = ruta + "/src/" + archivo;
+                                                    //Scanner
+                                                    Parser parse = new Parser();
+                                                    System.out.println("Debugging: Parse");
+                                                    parse.main(null,ruta);
+                                                    System.out.println("Parsing completado");
+                                                    break;
+                                                case "ast":
+                                                    ruta = System.getProperty("user.dir");
+                                                    ruta = ruta + "/src/" + archivo;
+                                                    //Scanner
+                                                    Ast ast = new Ast();
+                                                    System.out.println("Debugging: Parse");
+                                                    ast.main(null,ruta);
+                                                    System.out.println("Ast completado");
+                                                    break;
+                                                case "semantic":
+                                                    ruta = System.getProperty("user.dir");
+                                                    ruta = ruta + "/src/" + archivo;
+                                                    //Semantic
+                                                    Semantic semantic = new Semantic();
+                                                    System.out.println("Debugging: Semantic");
+                                                    semantic.main(null,ruta);
+                                                    System.out.println("semantic completado");
+                                                    break;
+                                                case "irt":
+                                                    ruta = System.getProperty("user.dir");
+                                                    ruta = ruta + "/src/" + archivo;
+                                                    //Semantic
+                                                    IRT_C irt = new IRT_C();
+                                                    System.out.println("Debugging: Irt");
+                                                    irt.main(null,ruta);   
+                                                    System.out.println("Irt completado");
+                                                    break;
+                                                case "codegen":
+                                                    break;
+                                            }
+                                        }
+                                        if(verificacion == -1){
+                                                 x = tag.length();
+                                         }
+                                        else if(verificacion2 == -1){
+                                             x = tag.length();
+                                        
+                                        }
+                                        else{
+                                              x = dos_puntos;
+                                        }
+                                      
+                                    }
                                 }
+                                
                             }
+                          
                             
                         }
                         
